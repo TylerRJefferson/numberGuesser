f7def1c45e255b29ea83d4fec4e12484dce5f035
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-13 09:34:30 -0400

    starting project

---
+++ guess.js
@@ -0,0 +1,8 @@
+/*
+Create a program that will generate a random number from 1-10,
+then asks us to guess that number.
+If our guess is higher, say "too high" and ask again.
+If it is lower, say "too low" and ask again.
+If it is the right number, say "congratulations" and exit
+*/
+
