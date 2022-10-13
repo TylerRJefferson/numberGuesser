693029efea1a54929fe1dabb7d298668c7a46bd7
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-13 10:33:23 -0400

    random number guessing game complete

--- guess.js
+++ guess.js
@@ -6,3 +6,29 @@ If it is lower, say "too low" and ask again.
 If it is the right number, say "congratulations" and exit
 */
 
+import readline from "readline-sync"; //import npm library to guess.js
+
+let userGuessedRight = false //setting variable to false boolean
+
+// while(true) { //just playing around, constantly generates random nums
+   // gives a random num from 1-10 and always rounds up
+  let NumberChosen = Math.ceil(Math.random() * 10)
+  // console.log(NumberChosen)
+// }
+
+while(!userGuessedRight){
+let guess = readline.question("What is your guess?") //while userGuessedRight variable is a falsy condition, tell us to keep guessing
+if (guess > NumberChosen) //if guess is higher than number chosen, tell us "too high"
+{
+    console.log("Too high!") 
+  }
+if (guess < NumberChosen) //if guess is lower than number chosen, tell us "too low"
+  {
+    console.log("Too low!")
+  }
+if (guess == NumberChosen) //if guess is the number chosen, tell us congrats
+  {
+    console.log("Congratulations!")
+    userGuessedRight = true
+  }
+}
